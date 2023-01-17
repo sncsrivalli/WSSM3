8ac6f9a049ef232dc3fa37254374cf45450fda48
QPS-Basavanagudi <QPS-Basavanagudi@DESKTOP-F52EDBA>
2023-01-17 12:23:02 +0530

    17/01/2023

---
+++ src/datadriventesting/ReadDataFromExcel.java
@@ -0,0 +1,49 @@
+package datadriventesting;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ReadDataFromExcel {
+
+	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+		//Step 1: Convert physical file into java readable object
+		// It throws FileNotFoundException
+		FileInputStream fis = new FileInputStream("./testdata/TD1.xlsx");
+		
+		//import Workbook, WorkbookFactory, Sheet, Row, Cell from org.apache.poi.ss.usermodel
+		
+		Workbook wb = null;
+		try {
+		//Step 2: Open excel workbook
+		//It throws EncryptedDocumentException, IOException
+		wb = WorkbookFactory.create(fis);
+		
+		//Step 3: Fetch required sheet
+		Sheet sheet = wb.getSheet("Sheet1");
+		
+		//Step 4: Fetch required row
+		Row row = sheet.getRow(0);
+		
+		//Step 5: Fetch required cell
+		Cell cell = row.getCell(1);
+		
+		//Step 6: Read data from cell
+		String data = cell.getStringCellValue();
+		System.out.println(data);
+		}
+		
+		finally {
+		//Step 7: Close workbook
+		wb.close();
+		}
+		
+	}
+
+}
---
+++ src/datadriventesting/WriteDataIntoExcel.java
@@ -0,0 +1,52 @@
+package datadriventesting;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class WriteDataIntoExcel {
+
+	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+		
+		// Step 1: Convert physical file into java readable object
+		// It throws FileNotFoundException
+		FileInputStream fis = new FileInputStream("./testdata/TD1.xlsx");
+
+		// import Workbook, WorkbookFactory, Sheet, Row, Cell from org.apache.poi.ss.usermodel
+		Workbook wb = null;
+		try {
+		// Step 2: Open excel workbook
+		// It throws EncryptedDocumentException, IOException
+		wb = WorkbookFactory.create(fis);
+
+		// Step 3: Fetch required sheet
+		Sheet sheet = wb.getSheet("Sheet1");
+
+		// Step 4: Fetch required row
+		Row row = sheet.getRow(0);
+
+		// Step 5: create required cell to edit
+		Cell cell = row.createCell(2);
+		
+		//Step 6: Set data into cell
+		cell.setCellValue("https://www.facebook.com/");
+		
+		//Step 7: Save data into excel
+		FileOutputStream fos = new FileOutputStream("./testdata/TD1.xlsx");
+		wb.write(fos);
+		}
+		
+		finally {
+		//Step 8: close workbook
+		wb.close();
+		}
+	}
+
+}
