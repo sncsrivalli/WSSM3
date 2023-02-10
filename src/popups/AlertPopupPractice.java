260f27b3c94178ca771be774c9bcc0ba1c00e560
QPS-Basavanagudi <QPS-Basavanagudi@DESKTOP-F52EDBA>
2023-02-10 18:15:23 +0530

    Comment added to AlertpopUp

--- src/popups/AlertPopupPractice.java
+++ src/popups/AlertPopupPractice.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+//Alert pop up
 public class AlertPopupPractice {
 
 	public static void main(String[] args) {
