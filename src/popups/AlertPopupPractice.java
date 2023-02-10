660b1fb5950d05f1062c9249568210efd872da39
QPS-Basavanagudi <QPS-Basavanagudi@DESKTOP-F52EDBA>
2023-02-10 18:16:35 +0530

    AlertPopupPractice

--- src/popups/AlertPopupPractice.java
+++ src/popups/AlertPopupPractice.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+//AlertPopupPractice
 public class AlertPopupPractice {
 
 	public static void main(String[] args) {
